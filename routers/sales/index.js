a97d55ef873e3233c69aa9004e23ed0994d6b0e0
LHenriquerios <henriquerios4@gmail.com>
2022-05-10 11:29:56 -0300

    feat: add validation of sale

--- routers/sales/index.js
+++ routers/sales/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { listSales, salesById } = require('../../controllers/sales');
+const { validationProductId, validationQuantity } = require('../../middlewares/validationSales');
 
 const salesRouter = express.Router();
 
 salesRouter.get('/', listSales);
 salesRouter.get('/:id', salesById);
+salesRouter.post('/', validationProductId, validationQuantity);
 
 module.exports = salesRouter;
