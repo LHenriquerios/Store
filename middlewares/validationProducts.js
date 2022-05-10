7b2ead5b126247a01b121bc7cca46a075eb6a5e4
LHenriquerios <henriquerios4@gmail.com>
2022-05-10 11:16:20 -0300

    fix: status code was incorret

--- middlewares/validationProducts.js
+++ middlewares/validationProducts.js
@@ -1,9 +1,9 @@
-const { UNAUTHORIZED, UNPROCESSABLE_ENTITY } = require('../statusCode');
+const { BAD_REQUEST, UNPROCESSABLE_ENTITY } = require('../statusCode');
 
 const validationName = async (req, res, next) => {
     const { name } = req.body;
 
-    if (!name) return res.status(UNAUTHORIZED).json({ message: '"name" is required' });
+    if (!name) return res.status(BAD_REQUEST).json({ message: '"name" is required' });
     if (name.length < 5) {
         return res.status(UNPROCESSABLE_ENTITY).json({
             message: '"name" length must be at least 5 characters long' });
@@ -19,7 +19,7 @@ const validationQuantity = async (req, res, next) => {
         return res.status(UNPROCESSABLE_ENTITY).json({
             message: '"quantity" must be greater than or equal to 1' });
         }
-if (!quantity) return res.status(UNAUTHORIZED).json({ message: '"quantity" is required' });
+if (!quantity) return res.status(BAD_REQUEST).json({ message: '"quantity" is required' });
 
     next();
 };
