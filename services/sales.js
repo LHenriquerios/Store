749ffd28a50e01cffa712d9b0f579c133399a910
LHenriquerios <henriquerios4@gmail.com>
2022-05-13 19:10:14 -0300

    feat: add test from sales

--- services/sales.js
+++ services/sales.js
@@ -28,7 +28,6 @@ const createSale = async (sale) => {
 const editSale = async (sale) => {
     const { id, productId, quantity } = sale;
     const editedSale = await modelSales.editSale(id, productId, quantity);
-    console.log(editedSale);
     return editedSale;
 };
 
